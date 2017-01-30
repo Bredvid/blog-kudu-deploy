7ab9dd6143e1936bf2e207281c40a1dc7314a74f
Vidar Kongsli <vidar@kongs.li>
2017-01-30 13:06:50 +0100

    Initial commit

---
+++ src/Bredvid.Blog.KuduDeploy.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bredvid.Blog.KuduDeploy.Web.Startup))]
+namespace Bredvid.Blog.KuduDeploy.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
