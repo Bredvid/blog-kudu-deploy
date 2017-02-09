caa879e94074c33f9bb98676cfe8ec842033caa0
Vidar Kongsli <vidar@kongs.li>
2017-02-09 13:41:38 +0100

    Blog post 2: add test project

---
+++ src/Bredvid.Blog.KuduDeploy.Tests/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Bredvid.Blog.KuduDeploy.Tests
+{
+	[TestFixture]
+    public class TestClass
+    {
+	    [Test]
+	    public void ShouldBeRun()
+	    {
+	    }
+    }
+}
